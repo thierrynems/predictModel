25dad4d521a67f92609d1a42cf671f89e9edfd08
KONGNE N. Thierry <thierrynemsk@gmail.com>
2020-11-04 12:58:16 +0100

    Update myMain.py
    update model to predict gene with different model

--- myMain.py
+++ myMain.py
@@ -42,6 +42,12 @@ paramDictModel = {
     'mode': 'max'
 }
 
+datasetNT = sys.argv[1]
+datasetAA = sys.argv[2]
+modelFile = sys.argv[3]
+
+#list des modeles
+listModel=("deeplyTrain9deeply-model.h5","sgsTrain9deeply-model.h5","deepHE9deeply-model.h5")
 #proportion 'trainingProp': 0.8,
 #'trainingProp': 0.8, //proportion data d'entrainement 
 
@@ -66,29 +72,11 @@ def getGeneFeatTableSeq(feat, geneName,classLabel=1):
             
     return featList
 
-#def getScaledData(dataMatrix):
- #       scaler = StandardScaler().fit(dataMatrix[:, np.newaxis])
- #       return scaler.transform(dataMatrix[:, np.newaxis])
+def getScaledData(dataMatrix):
+        scaler = StandardScaler().fit(dataMatrix[:, np.newaxis])
+        return scaler.transform(dataMatrix[:, np.newaxis])
         #return scaler.transform(dataMatrix)
 
-def getScaledData(dataMatrix):
-        scaler = StandardScaler().fit(dataMatrix)
-        return scaler.transform(dataMatrix)
-#######
-# returns the TP, TN, FP and FN values
-def getTPTNValuesPredict(test, testPred):
-    TP, TN, FP, FN = 0, 0, 0, 0
-    for i in range(len(testPred)):
-        if test[i] == testPred[i] == 1:
-            TP += 1
-        if testPred[i] == 1 and test[i] != testPred[i]:
-            FP += 1
-        if test[i] == testPred[i] == 0:
-            TN += 1
-        if testPred[i] == 0 and test[i] != testPred[i]:
-            FN += 1
-
-    return TP, TN, FP, FN
 #########################################################
 def main():
     essential_dir_path = "input/essential/"
@@ -108,42 +96,26 @@ def main():
     read = ReadFiles(param)
     read.getDatasetFileInfo()  # read the dataset for other method to access dataset info
     
-    #Dataset Essential Gene
-    datasetAA="input/Organisms/DesulfovibrioAlaskensis/E_essential_aa_dde.txt"
-    datasetNT="input/Organisms/DesulfovibrioAlaskensis/E_essential_gene_dde.txt"
-    
-    #Dataset Non essential Gene
-    datasetAA_NE="input/Organisms/DesulfovibrioAlaskensis/N_essential_aa_dde.txt"
-    datasetNT_NE="input/Organisms/DesulfovibrioAlaskensis/N_essential_gene_dde.txt"
+    # process the files and generate features from sequence and other files
+    #self, read,geneSeq,protSeq
 
-   # datasetAA="input/nonEssential/degaa-np.dat"
-   # datasetNT="input/nonEssential/degseq-np.dat"
-
-    OrganismName="OrganismSpecific"
-    Org="dde"
+    #datasetAA="input/nonEssential/degaa-np.dat"
+    #datasetNT="input/nonEssential/degseq-np.dat"
+    OrganismName="OrganismDeeply"
+    #Org="dde"
     #df = pandas.read_table(dataset,sep= '\t', header='infer')
-    baseDir="output/"
-    if os.path.isdir(baseDir): 
-    	print("le repertoire existe")
-    else: 
-    	print("Creation du repertoire "+ baseDir)
-    	os.mkdir(baseDir);
 
-    filename=os.path.join(baseDir, str(OrganismName)+".csv")
+    #baseDir="output/"
+    #if os.path.isdir(baseDir): 
+    #	print("le repertoire existe")
+    #else: 
+    #	print("Creation du repertoire "+ baseDir)
+    #	os.mkdir(baseDir);
+
+    filename=str(OrganismName)+".csv"
     file = open(filename, "a")
-    #Essential
     fastaSequencesNT = SeqIO.parse(open(datasetNT), 'fasta')
     fastaSequencesAA = SeqIO.parse(open(datasetAA), 'fasta')
-    #Non Essential 
-    fastaSequencesNT_NE = SeqIO.parse(open(datasetNT_NE), 'fasta')
-    fastaSequencesAA_NE = SeqIO.parse(open(datasetAA_NE), 'fasta')
-    #
-    EssentialListFeat = list()
-    sequenceLabel=list()
-    #Non Essential
-    N_EssentialListFeat = list()
-    N_essentialLabel=list()
-    #browse essential Gene 
     for fastaNT in fastaSequencesNT:
         nameNT, sequenceNT = fastaNT.id, str(fastaNT.seq)
         isSequence=0
@@ -164,159 +136,54 @@ def main():
             feat.getFeaturesSeq(paramDict['bins'],nameNT)
             #combiner toutes les features
             dataToPredict=getGeneFeatTableSeq(feat, nameNT)
-            EssentialListFeat.append(dataToPredict)
-            sequenceLabel.append(1)
+            myData=np.array(dataToPredict)
+            #myData.append(dataToPredict[:])
+            ####################################
+            #mydata=np.asarray(dataToPredict)
+            #mydata=mydata.reshape(1,-1) 
+            #print(myData)
+            #sys.exit()
+            ################################""""""
+            #myData=myData.reshape(1,-1) 
+            dataMatrix=getScaledData(myData)
+            dataMatrix=dataMatrix.reshape(1,-1)
+            #dataMatrix=dataMatrix.ravel()
+            #print("#########################################")
+            #print(dataMatrix)
+            #print("#########################################")
+            #sys.exit()
+            ##model de prediction 
+            ################################Old load model 3333 #######################################
+            
+            modelPath=listModel[modelFile]
+            loaded_model = load_model(modelPath)
+            resultPrediction=loaded_model.predict(dataMatrix)
+	   # print(resultPrediction.shape)
+	   # sys.exit()
+            E_score=resultPrediction[0][1]
+            NE_score=resultPrediction[0][0]
+            if E_score>=0.5: 
+                decision="E"
+            else: 
+                decision="NE"
+            #loaded_model.predict_classes
+            print(idSequenceNN+"=>"+resultPrediction)
+            testPredLabelRev = np.argmax(resultPrediction, axis=1)
+            print("argmax= "+str(testPredLabelRev))
+            print("\n")
+            line= str(Org)+";"+str(idSequenceNN)+";"+str(E_score)+";"+str(NE_score)+";"+str(decision)+"\n"
+            file.write(str(line))
+            ######################################################################
+            #prediction=loaded_model.predict_classes(dataMatrix)
+            #print(prediction)
+            #probs = loaded_model.predict_proba(dataMatrix)
+            #print(probs[:, 1])
 
-    #non Essential 
-    #browse essential Gene 
-    for fastaNT in fastaSequencesNT_NE:
-        nameNT, sequenceNT = fastaNT.id, str(fastaNT.seq)
-        isSequence=0
-       # idSequenceNN=nameNT[4:17]
-	idSequenceNN=nameNT
-        for fastaAA in fastaSequencesAA_NE: 
-            nameAA, sequenceAA = fastaAA.id, str(fastaAA.seq)
-           # idSequenceAA=nameAA[4:17]
-	    idSequenceAA=nameAA
-            if idSequenceAA == idSequenceNN :
-                isSequence=1
-                break;
-        #print("NT: "+idSequenceNN+" AA: "+idSequenceNN)
-        if isSequence == 1: # les deux sequence existe 
-            sequenceNT = sequenceNT.upper()
-            sequenceAA = sequenceAA.upper() 
-            feat = FeatureProcessingSeq(read,sequenceNT,sequenceAA)
-            feat.getFeaturesSeq(paramDict['bins'],nameNT)
-            #combiner toutes les features
-            dataToPredict=getGeneFeatTableSeq(feat, nameNT)
-            EssentialListFeat.append(dataToPredict)
-            sequenceLabel.append(0)
-
-    EssentialMatrixFeat = np.array(EssentialListFeat)
-    print(EssentialMatrixFeat.shape)
-    X_test = getScaledData(EssentialMatrixFeat)
-    numberOfClasses = encodeClassLabel(sequenceLabel)
-    testLabel = np_utils.to_categorical(sequenceLabel, numberOfClasses)
-    print(X_test)
-   # print(testLabel)
-    #load a model 
-    # sys.exit()
-    #load SGS0.1 
-    #loaded_model = load_model('sgsTrain9deeply-model.h5')
-
-    #load default deeplyEssential 
-    loaded_model = load_model('deeplyTrain9deeply-model.h5')
-    
-    #load DeeplyHE
-  # loaded_model = load_model('deepHE9deeply-model.h5')
-  #  loaded_model = load_model('AcinetobacterTrain9deeply-model.h5')
-
-     # evaluation scores
-    roc_auc = metrics.roc_auc_score(testLabel, loaded_model.predict(X_test))
-    precision = metrics.average_precision_score(testLabel, loaded_model.predict(X_test))
-    print("roc-auc= "+str(roc_auc)+ " Precision= "+str(precision))
-
-    # get predicted class label
-    probs = loaded_model.predict_proba(X_test)
-    testPredLabel = loaded_model.predict(X_test)
-    true_y = list()
-    for y_i in range(len(testLabel)):
-        true_y.append(testLabel[y_i][1])
-    probs = probs[:, 1]
-
-    fpr, tpr, threshold = metrics.roc_curve(true_y, probs)
-    #print("############################################")
-    #print(threshold)
-    # convert back class label from categorical to integer label
-    testLabelRev = np.argmax(testLabel, axis=1)
-    testPredLabelRev = np.argmax(testPredLabel, axis=1)
-    print("np.argmax(testPredLabel, axis=1)")
-    # get TP, TN, FP, FN to calculate sensitivity, specificity, PPV and accuracy
-    TP, TN, FP, FN = getTPTNValuesPredict(testLabelRev, testPredLabelRev)
-    #compute the different metrics
-    sensitivity = float(TP) / float(TP + FN)
-    specificity = float(TN) / float(TN + FP)
-    PPV = float(TP) / float(TP + FP)
-    accuracy = float(TP + TN) / float(TP + FP + FN + TN)
-
-    # dictionary to store evaluation stat
-    evaluationInfo = {
-        'roc_auc': roc_auc,
-        'precision': precision,
-        'sensitivity': sensitivity,
-        'specificity': specificity,
-        'PPV': PPV,
-        'accuracy': accuracy
-    }
-    print("######################################")
-    print(evaluationInfo)
-    sys.exit()
-    #myData=np.array(dataToPredict)  # a voir 
-    
-    #myData.append(dataToPredict[:])
-    ####################################
-    #mydata=np.asarray(dataToPredict)
-    #mydata=mydata.reshape(1,-1) 
-    #print(myData)
-    #sys.exit()
-    ################################""""""
-    #myData=myData.reshape(1,-1) 
-    dataMatrix=getScaledData(myData)
-    dataMatrix=dataMatrix.reshape(1,-1)
-    #dataMatrix=dataMatrix.ravel()
-    #print("#########################################")
-    #print(dataMatrix)
-    #print("#########################################")
-    #sys.exit()
-    ##model de prediction 
-    ################################Old load model 3333 #######################################
-    
-    #json_file = open('deeply-model.json', 'r')
-    #loaded_model_json = json_file.read()
-    #json_file.close()
-    #loaded_model = model_from_json(loaded_model_json)
-    # load best model and compile
-    
-    #loaded_model.load_weights('weights.best.hdf5')
-    #loaded_model.compile(optimizer=optimizerDict['adadelta'],
-     #             loss=paramDictModel['loss'],
-     #             metrics=paramDictModel['metrics']) sgsTrain9deeply-model.h5
-
-    #load a model 
-    loaded_model = load_model('sgsTrain9deeply-model.h5')
-    resultPrediction=loaded_model.predict(dataMatrix)
-    probs = loaded_model.predict_proba(dataMatrix)
-    print(probs)
-    probs = probs[:, 1]
-    true_y=list()
-    true_y.append(1)
-    fpr, tpr, threshold = metrics.roc_curve(true_y, probs)
-    print("fpr= "+str(fpr)+", "+str(tpr)+ ","+ str(threshold))
-    #sys.exit()
-# print(resultPrediction.shape)
-# sys.exit()
-    E_score=resultPrediction[0][1]
-    NE_score=resultPrediction[0][0]
-    if E_score>=0.5: 
-        decision="E"
-    else: 
-        decision="NE"
-    #loaded_model.predict_classes
-    print(resultPrediction)
-    print("\n")
-    line= str(Org)+";"+str(idSequenceNN)+";"+str(E_score)+";"+str(NE_score)+";"+str(decision)+"\n"
-    file.write(str(line))
-    ######################################################################
-    #prediction=loaded_model.predict_classes(dataMatrix)
-    #print(prediction)
-    #probs = loaded_model.predict_proba(dataMatrix)
-    #print(probs[:, 1])
-
-    ###################################################################
-    #testLabelRev = np.argmax(resultPrediction, axis=1)
-    #print(testLabelRev)
-    #probs = loaded_model.predict_proba(mydata)
-    #print(probs)
+            ###################################################################
+            #testLabelRev = np.argmax(resultPrediction, axis=1)
+            #print(testLabelRev)
+            #probs = loaded_model.predict_proba(mydata)
+            #print(probs)
     file.close()
     sys.exit()
     # get features and build training/testing dataset
