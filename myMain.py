44620d88c056258f99e4392e5654221db96be1d6
KONGNE N. Thierry <thierrynemsk@gmail.com>
2020-11-05 21:34:41 +0100

    Update myMain.py
    cast type

--- myMain.py
+++ myMain.py
@@ -44,7 +44,7 @@ paramDictModel = {
 
 datasetNT = sys.argv[1]
 datasetAA = sys.argv[2]
-modelFile = sys.argv[3]
+modelFile = int(sys.argv[3])
 
 #list des modeles
 listModel=("deeplyTrain9deeply-model.h5","sgsTrain9deeply-model.h5","deepHE9deeply-model.h5")
